38d1754bd531eda01f272ee94faea8e89399b810
Vraj-Roy <58093447+Vraj-Roy@users.noreply.github.com>
2024-08-06 14:16:56 +0530

    second

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,14 @@
 import Left from "./Left";
 import Middle from "./Middle";
-const Main = () =>{
-    return (
-        <>
-        <div className="flex">  
-    <Left  />  
- <div className="h-full w-[20vw]"></div>
-    <Middle /> 
-</div>
-
-        </>
-    )
-}
+const Main = () => {
+  return (
+    <>
+      <div className="flex">
+        <Left />
+        <div className="h-full w-[20vw] hidden md:block"></div>
+        <Middle />
+      </div>
+    </>
+  );
+};
 export default Main;
--- src/components/Middle.js
+++ src/components/Middle.js
@@ -2,13 +2,13 @@ import Feed from "./Feed";
 import NavBar from "./NavBar";
 import Stories from "./Stories";
 
-const Middle = () =>{
-return (
-    <div className="w-[80%] bg-black">
-    <NavBar/>
-    <Stories/>
-    <Feed/>
+const Middle = () => {
+  return (
+    <div className="w-full md:w-[80%] bg-black">
+      <NavBar />
+      <Stories />
+      <Feed />
     </div>
-)
-}
+  );
+};
 export default Middle;
