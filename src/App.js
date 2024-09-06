86d3d49f6e7136fbe8f81094a3d39f43d6c379be
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-06 23:51:26 +0530

    top loading bar added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Left from "./components/Left";
 import Middle from "./components/Middle";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Explore from "./components/options/Explore";
 import Saved from "./components/options/Saved";
 import Bottom from "./components/Bottom";
 import Reels from "./components/options/Reels";
 import Activity from "./components/options/Activity";
 import Settings from "./components/options/Settings";
+import LoadingBar from "react-top-loading-bar";
+
 function App() {
+  const [progress, setProgress] = useState(0);
+  const location = useLocation(); // Now useLocation is within the Router context
+
+  useEffect(() => {
+    // Function to run on route change
+    setProgress(100); // Simulates loading progress on route change
+  }, [location]); // Re-run when location changes
+
   return (
     <>
+      {/* // color="#f11946" */}
+      <LoadingBar
+        style={{
+          backgroundImage:
+            "linear-gradient(to right, #7e22ce, #b91c1c,#facc15)", // Replace with your desired gradient colors
+        }}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+        height={3}
+      />
+
       <div className="flex">
-        <BrowserRouter>
-          <Left />
-          <div className="h-full w-[20vw] hidden md:block"></div>
-          <Routes>
-            <Route path="/" element={<Middle />}></Route>
-            <Route path="/explore" element={<Explore />}></Route>
-            <Route path="/activity" element={<Activity />}></Route>
-            <Route path="/reels" element={<Reels />}></Route>
-            <Route path="/saved" element={<Saved />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-          </Routes>
-          <Bottom />
-        </BrowserRouter>
+        <Left />
+        <div className="h-full w-[20vw] hidden md:block"></div>
+        <Routes>
+          <Route path="/" element={<Middle />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/activity" element={<Activity />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+        <Bottom />
       </div>
       <div className="mt-14"></div>
     </>
   );
 }
 
-export default App;
+// Wrapping App component with BrowserRouter
+export default function AppWithRouter() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
